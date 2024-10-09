982683d639400ffde46610da5563da0c356a0cbf
programmerhasan001 <programmermdhasan@gmail.com>
2024-10-09 15:14:54 +0600

    first commit

---
+++ app/api/upload/route.js
@@ -0,0 +1,22 @@
+import { writeFile, readdir } from "fs/promises"
+import { NextResponse } from "next/server"
+import path from 'path'
+
+export const POST = async (request) => {
+    const file = await request.formData()
+    const image = file.get("image")
+    const byteLength = await image.arrayBuffer()
+    const bufferData = await Buffer.from(byteLength)
+    // const pathOfImage = `./public/${new Date().toDateString().split(" ").join("-")}${path.extname(image.name)}`
+    const pathOfImage = `./public/images/${new Date().getTime()}${path.extname(image.name)}`
+
+    writeFile(pathOfImage, bufferData)
+    console.log(pathOfImage)
+    return NextResponse.json({ msg: "success" })
+}
+
+
+export const GET = async (request) => {
+    const files = await readdir("./public/images");
+    return NextResponse.json({ msg: "image get successfully", files })
+}
